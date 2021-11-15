104aa1510959f26d28e0484cca6bbdcedc785c92
Shibhansh <shibhanshdohare1997@gmail.com>
2021-11-15 14:37:41 -0700

    update/plot file

--- adult/plot.py
+++ adult/plot.py
@@ -7,12 +7,14 @@ from utils import *
 import matplotlib.pyplot as plt
 
 
-def add_cfg_performance(cfg=''):
+def add_cfg_performance(cfg='', settings=None):
     with open(cfg, 'r') as f:
         params = json.load(f)
     list_params, param_settings = get_configurations(params=params)
     per_param_setting_performance = []
-    for idx in range(len(param_settings)):
+    if settings is None:
+        settings = [i for i in range(len(param_settings))]
+    for idx in settings:
         file = params['data_dir'] + str(idx) + '/0'
         with open(file, 'rb') as f:
             data = pickle.load(f)
@@ -26,19 +28,33 @@ def add_cfg_performance(cfg=''):
 def main(arguments):
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--cfg_file', help="Path of the file containing the parameters of the experiment", type=str,
-                            default='cfg/gap_reg.json')
+                            default='cfg/l2_gap.json')
     args = parser.parse_args(arguments)
     cfg_file = args.cfg_file
 
     with open(cfg_file, 'r') as f:
         params = json.load(f)
-
     list_params, param_settings = get_configurations(params=params)
 
-    performances = add_cfg_performance(cfg=cfg_file)
 
-    plt.plot(performances[:, 1], performances[:, 0])
+    performances = add_cfg_performance(cfg=cfg_file, settings=[i for i in range(0, 5)])
+    plt.plot(performances[:, 1], performances[:, 0], label=params[list_params[0]][0])
+
+    performances = add_cfg_performance(cfg=cfg_file, settings=[i for i in range(5, 10)])
+    plt.plot(performances[:, 1], performances[:, 0], label=params[list_params[0]][1])
+
+    performances = add_cfg_performance(cfg=cfg_file, settings=[i for i in range(10, 15)])
+    plt.plot(performances[:, 1], performances[:, 0], label=params[list_params[0]][2])
+
+    performances = add_cfg_performance(cfg=cfg_file, settings=[i for i in range(15, 20)])
+    plt.plot(performances[:, 1], performances[:, 0], label=params[list_params[0]][3])
+
+    performances = add_cfg_performance(cfg='cfg/gap_reg.json', settings=[i for i in range(0, 5)])
+    plt.plot(performances[:, 1], performances[:, 0], label='0')
+
+    plt.legend()
     plt.show()
+    plt.savefig('cmpr.png')
 
 
 if __name__ == '__main__':
